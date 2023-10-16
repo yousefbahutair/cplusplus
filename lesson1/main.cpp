42ca69b09d52da1be83a5e53483f07d04591a530
yousef nasser <joonasser480@gmail.com>
2023-10-16 20:54:35 +0400

    changing code

--- lesson1/main.cpp
+++ lesson1/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
-using namespace std;
+int main()
+{
+    int main = 0;
 
-int main() {
-    cout << "Hello world!" << endl;
-    return 0;
-} 
+int your_name;
+int your_age;
+std :: cout << "Enter your name"; 
+std :: cin >> your_name;
+std :: cout << " that's my brother name";
+std :: cout << "Enter your age";
+std :: cin >> your_age;
+std :: cout << "cool! you are as old as I am" <<std::endl;
+
+return 0;
+}
