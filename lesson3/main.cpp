41b1ebc426d783720d9ca3aa4c1ec2bd0b0ce561
yousef nasser <joonasser480@gmail.com>
2023-10-17 12:59:11 +0400

    feat(new_file_creating_calculator)

---
+++ lesson3/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char op;
+    float number1;
+    float number2;
+    cout << "Enter first number:";
+    cin >> number1;
+    cout << "Enter the process: +, -, *, / :";
+    cin >> op;
+    cout << "Enter another number:";
+    cin >> number2;
+    switch (op) { 
+        case '+':
+        cout << number1 + number2;
+        break;
+        case '-' :
+        cout << number1 -number2;
+        break;
+        case '*':
+        cout << number1 * number2;
+        break;
+        case '/':
+        cout << number1 / number2;
+        break;
+        
+        default:
+        cout << "Error";
+        break;
+        
+    }
+
+    return 0;
+}
