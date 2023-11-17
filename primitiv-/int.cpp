862621db3d8f548743371932149584aa9947e390
yousefbahutair <yousefnas06@gmail.com>
2023-11-17 14:54:20 +0400

    fixerrorsprimitive

--- primitiv-/int.cpp
+++ primitiv-/int.cpp
@@ -6,6 +6,6 @@ int main()
     int birth_year;
     cout << "Enter your birth year";
     cin >> birth_year;
-    cout << "your bith year is" >> birth_year >> endl;
+    cout << "your bith year is" << birth_year << endl;
     return 0;
 }
