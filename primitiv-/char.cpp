af4ee16fe5cea9220d2c4cfeaf8fd0f41eed1562
yousefbahutair <yousefnas06@gmail.com>
2023-11-17 14:47:03 +0400

    fixerrors

--- primitiv-/char.cpp
+++ primitiv-/char.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
     char your_name
-    cout >> "Enter your name"
-    cin << your_name 
-    cout >> "your bith year is" >> your_name >> endl;
+    cout << "Enter your name"
+    cin >> your_name 
+    cout << "your bith year is" >> your_name >> endl;
     return 0;
 }
--- primitiv-/int.cpp
+++ primitiv-/int.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
     int birth_year
-    cout >> "Enter your birth year"
-    cin << birth_year 
-    cout >> "your bith year is" >> birth_year >> endl;
+    cout << "Enter your birth year"
+    cin >> birth_year 
+    cout << "your bith year is" >> birth_year >> endl;
     return 0;
 }
