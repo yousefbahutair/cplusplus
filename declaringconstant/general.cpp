ccdfaf614f0fb230577cfc4de5c8f30306f99bf0
yousefbahutair <yousefnas06@gmail.com>
2023-11-18 03:13:28 +0400

    general_ideas_about_declaring_and_using_constants

--- declaringconstant/general.cpp
+++ declaringconstant/general.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <string> 
 
 using namespace std;
 
 int main()
 {
-    char large_or_small;
+    string large_or_small;
     int number_of_rooms{0};
     int number_large{0};
     int numbersmall{0};
@@ -18,40 +19,35 @@ int main()
     cout << "_____________________________________" << endl;
     cout << "Would you like to take a large room or small room? (large/small/both) ";
     cin >> large_or_small;
-    switch (large_or_small)
+    if (large_or_small == "large" || large_or_small == "Large")
     {
-        case 'large':
-        case 'Large':
-            cout << "How many rooms do you want? ";
-            cin >> number_of_rooms;
-            cout << "Price for all rooms is $" << number_of_rooms * large_room_per_dirhams << endl;
-            cout << "Your sales taxes is $" << number_of_rooms * large_room_per_dirhams * sale_tax << endl;
-            cout << "Your total estimate is $" << (number_of_rooms * large_room_per_dirhams * sale_tax) + (number_of_rooms * large_room_per_dirhams) << endl;
-            break;
-
-        case 'small':
-        case 'Small':
-            cout << "How many rooms would you like to take? ";
-            cin >> number_of_rooms;
-            cout << "Price for all rooms is $" << number_of_rooms * small_room_per_dirhams << endl;
-            cout << "Your taxes is $" << number_of_rooms * small_room_per_dirhams * sale_tax << endl;
-            cout << "Your total estimate is $" << (number_of_rooms * small_room_per_dirhams * sale_tax) + (number_of_rooms * small_room_per_dirhams) << endl;
-            break;
-
-        case 'both':
-        case 'Both':
-            cout << "How many large rooms do you want to take? ";
-            cin >> number_large;
-            cout << "And how many small rooms do you want? ";
-            cin >> numbersmall;
-            cout << "Price for all small and large rooms is $" << (numbersmall * small_room_per_dirhams) + (number_large * large_room_per_dirhams) << endl;
-            cout << "Your taxes is $" << ((numbersmall * small_room_per_dirhams) + (number_large * large_room_per_dirhams)) * sale_tax << endl;
-            cout << "Your total estimate is $" << (((numbersmall * small_room_per_dirhams) + (number_large * large_room_per_dirhams)) * sale_tax) + (numbersmall * small_room_per_dirhams) + (number_large * large_room_per_dirhams) << endl;
-            break;
-
-        default:
-            cout << "Invalid input";
-            break;
+        cout << "How many rooms do you want? ";
+        cin >> number_of_rooms;
+        cout << "Price for all rooms is $" << number_of_rooms * large_room_per_dirhams << endl;
+        cout << "Your sales taxes is $" << number_of_rooms * large_room_per_dirhams * sale_tax << endl;
+        cout << "Your total estimate is $" << (number_of_rooms * large_room_per_dirhams * sale_tax) + (number_of_rooms * large_room_per_dirhams) << endl;
+    }
+    else if (large_or_small == "small" || large_or_small == "Small") 
+    {
+        cout << "How many rooms would you like to take? ";
+        cin >> number_of_rooms;
+        cout << "Price for all rooms is $" << number_of_rooms * small_room_per_dirhams << endl;
+        cout << "Your taxes is $" << number_of_rooms * small_room_per_dirhams * sale_tax << endl;
+        cout << "Your total estimate is $" << (number_of_rooms * small_room_per_dirhams * sale_tax) + (number_of_rooms * small_room_per_dirhams) << endl;
+    }
+    else if (large_or_small == "both" || large_or_small == "Both")
+    {
+        cout << "How many large rooms do you want to take? ";
+        cin >> number_large;
+        cout << "And how many small rooms do you want? ";
+        cin >> numbersmall;
+        cout << "Price for all small and large rooms is $" << (numbersmall * small_room_per_dirhams) + (number_large * large_room_per_dirhams) << endl;
+        cout << "Your taxes is $" << ((numbersmall * small_room_per_dirhams) + (number_large * large_room_per_dirhams)) * sale_tax << endl;
+        cout << "Your total estimate is $" << (((numbersmall * small_room_per_dirhams) + (number_large * large_room_per_dirhams)) * sale_tax) + (numbersmall * small_room_per_dirhams) + (number_large * large_room_per_dirhams) << endl;
+    }
+    else
+    {
+        cout << "Invalid input";
     }
 
     return 0;
