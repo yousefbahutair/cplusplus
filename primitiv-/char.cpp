42abb74fb6523ee7a78861e61c635263bc3ea947
yousefbahutair <yousefnas06@gmail.com>
2023-11-17 14:42:50 +0400

    primitve

---
+++ primitiv-/char.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char your_name
+    cout >> "Enter your name"
+    cin << your_name 
+    cout >> "your bith year is" >> your_name >> endl;
+    return 0;
+}
