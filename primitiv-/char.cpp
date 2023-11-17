128bc8629adf24e5ac71960d1be38f522d688ed3
yousefbahutair <yousefnas06@gmail.com>
2023-11-17 14:48:55 +0400

    fixerrors

--- primitiv-/char.cpp
+++ primitiv-/char.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    char your_name
+    char your_name;
     cout << "Enter your name"
     cin >> your_name 
     cout << "your bith year is" >> your_name >> endl;
--- primitiv-/int.cpp
+++ primitiv-/int.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int birth_year
+    int birth_year;
     cout << "Enter your birth year"
     cin >> birth_year 
     cout << "your bith year is" >> birth_year >> endl;
