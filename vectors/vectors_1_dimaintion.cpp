71169c6a7712ffb3a2be8a036f260f2ee7dec4d4
yousefbahutair <yousefnas06@gmail.com>
2023-11-21 08:49:34 +0400

    vectors

--- vectors/vectors_1_dimaintion.cpp
+++ vectors/vectors_1_dimaintion.cpp
@@ -1,46 +1,45 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
 int main()
 {
-    vector <int> moon_years(3)
-    vector <int> moon_years(3){10000,572,700}
-    cout << " Our moon born in" moon_years[0] << "bc"<< endl;
-    cout << "and it split into two halves in" << moon_years[1] << "AD" << endl;
-    cout << "And now it looks like it does today since "  << moon_years(2) <<  "AD" << endl;
-    cout << "there are" << moon_years.size() << "years in victor" << endl;
+    vector<int> moon_years(3);
+    vector<int> moon_years2{10000, 572, 700};
+    cout << "Our moon was born in " << moon_years[0] << " BC" << endl;
+    cout << "and it split into two halves in " << moon_years[1] << " AD" << endl;
+    cout << "And now it looks like it does today since " << moon_years[2] << " AD" << endl;
+    cout << "There are " << moon_years.size() << " years in the vector" << endl;
 
-    vector <int> moon_years(3)// another way
-    vector <int> moon_years(3){10000,572,700}
-    cout << " Our moon born in" moon_years.at(0) << "bc"<< endl;
-    cout << "and it split into two halves in" << moon_years.at(1) << "AD" << endl;
-    cout << "And now it looks like it does today since "  << moon_years.at(2) << "AD" ;
-    cout << "if you wnat o change the years enter 3 years"
-    cin >> moons_years[0]
-    cin >> moons_years[1]
-    cin>> moons_years[2]
-    cout <<"new context:"<< endl;
-    cout << " Our moon born in" moon_years[0] << "bc"<< endl;
-    cout << "and it split into two halves in" << moon_years[1] << "AD" << endl;
-    cout << "And now it looks like it does today since "  << moon_years(2) <<  "AD" << endl;
-    cout << "there are" << moon_years.size() << "years in victor" << endl;
-    cout << " type new years if ou want to add new years" << endl;
-    int new_years
-    cout << "type the first year:"<< endl;
+    vector<int> moon_years3(3);
+    vector<int> moon_years4{10000, 572, 700};
+    cout << "Our moon was born in " << moon_years.at(0) << " BC" << endl;
+    cout << "and it split into two halves in " << moon_years.at(1) << " AD" << endl;
+    cout << "And now it looks like it does today since " << moon_years.at(2) << " AD" << endl;
+    cout << "If you want to change the years, enter 3 years:" << endl;
+    cin >> moon_years[0];
+    cin >> moon_years[1];
+    cin >> moon_years[2];
+    cout << "New context:" << endl;
+    cout << "Our moon was born in " << moon_years[0] << " BC" << endl;
+    cout << "and it split into two halves in " << moon_years[1] << " AD" << endl;
+    cout << "And now it looks like it does today since " << moon_years[2] << " AD" << endl;
+    cout << "There are " << moon_years.size() << " years in the vector" << endl;
+    cout << "Type new years if you want to add new years" << endl;
+    int new_years;
+    cout << "Type the first year:" << endl;
     cin >> new_years;
     moon_years.push_back(new_years);
-    cout << "type another year:" << endl;
-    cin << new_years;
+    cout << "Type another year:" << endl;
+    cin >> new_years;
     moon_years.push_back(new_years);
-    cout << " Our moon born in" moon_years[0] << "bc"<< endl;
-    cout << "and it split into two halves in" << moon_years[1] << "AD" << endl;
-    cout << "And now it looks like it does in"  << moon_years(2) <<  "AD" << endl;
-    cout << "And it became more acssessable to people since technogoly improved in" << moon_years[3] << "AD" << endl;
-    cout << " most of people now live in moon since " << moon_years[4] << "AD" << endl;
-    cout << "there are" << moon_years.size() << "years in victor" << endl;
-    cout << " type new years if ou want to add new years" << endl;
-
+    cout << "Our moon was born in " << moon_years[0] << " BC" << endl;
+    cout << "and it split into two halves in " << moon_years[1] << " AD" << endl;
+    cout << "And now it looks like it does in " << moon_years[2] << " AD" << endl;
+    cout << "And it became more accessible to people since technology improved in " << moon_years[3] << " AD" << endl;
+    cout << "Most people now live on the moon since " << moon_years[4] << " AD" << endl;
+    cout << "There are " << moon_years.size() << " years in the vector" << endl;
+    cout << "Type new years if you want to add new years" << endl;
 
     return 0;
 }
--- vectors/victor2dimaintion.cpp
+++ vectors/victor2dimaintion.cpp
@@ -4,40 +4,38 @@ using namespace std;
 
 int main()
 {
-    vector <vector<int>> buyers {
-        {1,2,3}
-        {4,5,6}
+    vector<vector<int>> buyers {
+        {1,2,3},
+        {4,5,6},
         {7,8,9}
-    }
-    cout << "1 percent of buyers rate the product" << buyers[0][0] << endl;
-    cout << "9 percent of buyers rate the product" << buyers[0][1] << endl;  
-    cout << "10 percent of buyers rate the product" << buyers[0][2] << endl;  
-    cout << "20 percent of buyers rate the product" << buyers[1][0] << endl;  
-    cout << "20 percent of buyers rate the product" << buyers[1][1] << endl;  
-    cout << "10 percent of buyers rate the product" << buyers[1][2] << endl;  
-    cout << "15 percent of buyers rate the product" << buyers[2][0] << endl;  
-    cout << "7.5 percent of buyers rate the product" << buyers[2][1] << endl;
-    cout << "7.5 percent of buyers rate the product" << buyers[2][2] << endl;
-    cout << "There are" << buyers.size() << " of percentage" << endl;
+    };
+    cout << "1 percent of buyers rate the product: " << buyers[0][0] << endl;
+    cout << "9 percent of buyers rate the product: " << buyers[0][1] << endl;  
+    cout << "10 percent of buyers rate the product: " << buyers[0][2] << endl;  
+    cout << "20 percent of buyers rate the product: " << buyers[1][0] << endl;  
+    cout << "20 percent of buyers rate the product: " << buyers[1][1] << endl;  
+    cout << "10 percent of buyers rate the product: " << buyers[1][2] << endl;  
+    cout << "15 percent of buyers rate the product: " << buyers[2][0] << endl;  
+    cout << "7.5 percent of buyers rate the product: " << buyers[2][1] << endl;
+    cout << "7.5 percent of buyers rate the product: " << buyers[2][2] << endl;
+    cout << "There are " << buyers.size() << " percentages." << endl;
 
-     int your_rating
-     cout << "enter your rating out of 10:" << endl;
-     cin >> your rating;
-     buyers.push_back(your_rating);
-     cout << "new ratings: " endl;
-     cout << "1 percent of buyers rate the product" << buyers[0][0] << endl;
-    cout << "9 percent of buyers rate the product" << buyers[0][1] << endl;  
-    cout << "10 percent of buyers rate the product" << buyers[0][2] << endl;  
-    cout << "20 percent of buyers rate the product" << buyers[1][0] << endl;  
-    cout << "20 percent of buyers rate the product" << buyers[1][1] << endl;  
-    cout << "10 percent of buyers rate the product" << buyers[1][2] << endl;  
-    cout << "15 percent of buyers rate the product" << buyers[2][0] << endl;  
-    cout << "7.5 percent of buyers rate the product" << buyers[2][1] << endl;
-    cout << "7.5 percent of buyers rate the product" << buyers[2][2] << endl;
-    cout << "your rating :" << your_rating << endl;
-    cout << "There are" << buyers.size() << " of percentage" << endl;
-
-
+     int your_rating;
+     cout << "Enter your rating out of 10:" << endl;
+     cin >> your_rating;
+     buyers.push_back(vector<int>{your_rating});
+     cout << "New ratings: " << endl;
+     cout << "1 percent of buyers rate the product: " << buyers[0][0] << endl;
+    cout << "9 percent of buyers rate the product: " << buyers[0][1] << endl;  
+    cout << "10 percent of buyers rate the product: " << buyers[0][2] << endl;  
+    cout << "20 percent of buyers rate the product: " << buyers[1][0] << endl;  
+    cout << "20 percent of buyers rate the product: " << buyers[1][1] << endl;  
+    cout << "10 percent of buyers rate the product: " << buyers[1][2] << endl;  
+    cout << "15 percent of buyers rate the product: " << buyers[2][0] << endl;  
+    cout << "7.5 percent of buyers rate the product: " << buyers[2][1] << endl;
+    cout << "7.5 percent of buyers rate the product: " << buyers[2][2] << endl;
+    cout << "Your rating: " << your_rating << endl;
+    cout << "There are " << buyers.size() << " percentages." << endl;
  
    return 0;
 }
