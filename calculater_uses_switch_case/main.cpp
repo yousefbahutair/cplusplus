86df59232cb05a700f77c64c029604dacc72204f
yousefbahutair <yousefnas06@gmail.com>
2023-11-27 13:30:09 +0400

    newfeat

---
+++ calculater_uses_switch_case/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char op;
+    float number1;
+    float number2;
+    cout << "Enter the operation with spaces:";
+    cin >> number1;
+    cin >> op;
+    cin >> number2;
+    switch (op) { 
+        case '+':
+        cout << number1 + number2;
+        break;
+        case '-' :
+        cout << number1 -number2;
+        break;
+        case '*':
+        cout << number1 * number2;
+        break;
+        case '/':
+        cout << number1 / number2;
+        break;
+        
+        default:
+        cout << "Error";
+        break;
+        
+    }
+
+    return 0;
+}
