dabc45e62e0bdb401c80c0a986988e97efbf046e
yousefbahutair <yousefnas06@gmail.com>
2023-11-27 12:40:15 +0400

    newfeat

--- if_statements/if.cpp
+++ if_statements/if.cpp
@@ -1,41 +1,43 @@
+Here is the corrected code:
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    double grade;
+    int grade;
     cout << "Enter your score in the exam: " << endl;
     cin >> grade;
     
     if (grade > 90)
     {
-        if (grade >= 95)
+        if (grade > 95)
             cout << "Your score is A+" << endl;
-        else if (grade < 95)
+        else if (grade <= 95)
             cout << "Your score is A" << endl;
     }
     
     if (grade > 80)
     {
-        if (grade >= 85)
+        if (grade > 85)
             cout << "Your score is B+" << endl;
-        else if (grade < 85)
+        else if (grade <= 85)
             cout << "Your score is B" << endl;
     }
     
     if (grade > 70)
     {
-        if (grade >= 75)
+        if (grade > 75)
             cout << "Your score is C+" << endl;
-        else if (grade < 75)
+        else if (grade <= 75)
             cout << "Your score is C" << endl;
     }
     
     if (grade > 60)
     {
-        if (grade >= 65)
+        if (grade > 65)
             cout << "Your score is D+" << endl;
-        else if (grade < 65)
+        else if (grade <= 65)
             cout << "Your score is D" << endl;
     }
     
