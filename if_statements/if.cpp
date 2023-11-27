cb395b6cbb2b66bb974d7194c88dac071366f086
yousefbahutair <yousefnas06@gmail.com>
2023-11-27 12:36:28 +0400

    newfeat

--- if_statements/if.cpp
+++ if_statements/if.cpp
@@ -3,34 +3,44 @@ using namespace std;
 
 int main()
 {
-    int grade;
-    cout <<"Enter your score in the exam"<< endl;
+    double grade;
+    cout << "Enter your score in the exam: " << endl;
     cin >> grade;
+    
     if (grade > 90)
-    { if (grade > 95)
-        cout << "Your score is A+" << endl;
+    {
+        if (grade >= 95)
+            cout << "Your score is A+" << endl;
         else if (grade < 95)
             cout << "Your score is A" << endl;
     }
+    
     if (grade > 80)
-    { if (grade > 85)
-        cout << "Your score is B+" << endl;
+    {
+        if (grade >= 85)
+            cout << "Your score is B+" << endl;
         else if (grade < 85)
             cout << "Your score is B" << endl;
     }
+    
     if (grade > 70)
-    { if (grade > 75)
-        cout << "Your score is C+" << endl;
+    {
+        if (grade >= 75)
+            cout << "Your score is C+" << endl;
         else if (grade < 75)
             cout << "Your score is C" << endl;
     }
+    
     if (grade > 60)
-    { if (grade > 65)
-        cout << "Your score is D+" << endl;
+    {
+        if (grade >= 65)
+            cout << "Your score is D+" << endl;
         else if (grade < 65)
             cout << "Your score is D" << endl;
     }
+    
     if (grade < 60)
-        cout << " Your score is F and you failed in the exam" << endl;
+        cout << "Your score is F and you failed in the exam" << endl;
+    
     return 0;
 }
