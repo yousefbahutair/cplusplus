d65c44a05a2d87c823a111c5e208a8b34737a291
yousefbahutair <yousefnas06@gmail.com>
2023-11-29 07:44:58 +0400

    forloop

---
+++ forloop/forloop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int number{2}; number<=10;++number)
+    { if(number/2==0)
+    cout << number << endl;
+    else
+    cout << ""<< endl;
+    
+    
+    }
+        
+    return 0;
+}
