fe8a8d4c2071146b1b4fca0963cdf874986e23e4
yousefbahutair <yousefnas06@gmail.com>
2023-12-01 19:15:35 +0400

    whileloop

--- whileloop/whileloop.cpp
+++ whileloop/whileloop.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     int number;
-    cout << "Enter a number less than 100 and we'll tell you whether it's odd or even : "<< endl;
+    cout << "Enter a number less than 100 and we'll tell you whether it's odd or even: " << endl;
     cin >> number;
-    while( number<=100)
-    { if(number %2 ==0)
-    cout << "Even";
-    else
-    cout << "Odd";
-    break;}
+    if (number < 100) {
+        if (number % 2 == 0) {
+            cout << "Even";
+        } else {
+            cout << "Odd";
+        }
+    } else {
+        cout << "try again";
+    }
     
     return 0;
 }
