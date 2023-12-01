35b9c469052400b6c153fda7bebfe8cb1b3d9e70
yousefbahutair <yousefnas06@gmail.com>
2023-12-01 09:44:51 +0400

    fixerrorswhileloop

--- whileloop/whileloop.cpp
+++ whileloop/whileloop.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int main()
 {
-    int number = 0;
+    int number;
     cout << "Enter a number less than 100 and we'll tell you whether it's odd or even : "<< endl;
+    cin >> number;
     while( number<=100)
     { if(number %2 ==0)
     cout << "Even";
