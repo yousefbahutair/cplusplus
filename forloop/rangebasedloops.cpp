4ff8bc0ac9b15529ad7721a1fd395a01fe56e788
yousefbahutair <yousefnas06@gmail.com>
2023-12-01 08:44:26 +0400

    forloop

--- forloop/rangebasedloops.cpp
+++ forloop/rangebasedloops.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main()
 { vector<int> dgrees;
 for( auto dgree: dgrees)
-cout << " Enter 4 dgrees" << dgree << endl;
+cout << "Enter 4 dgrees" << endl;
+cin >> dgrees;
+cout << " Enter 4 dgrees" << dgrees << endl;
 
             
     return 0;
