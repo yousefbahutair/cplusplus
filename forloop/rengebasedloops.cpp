193215ada59e83ca58a85c1ad60cbbd8c623ae6e
yousefbahutair <yousefnas06@gmail.com>
2023-12-01 08:35:56 +0400

    forloop

---
+++ forloop/rengebasedloops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{ vector<int> dgrees;
+for( auto dgree: dgrees)
+cout << " Enter 4 dgrees" << dgree << endl;
+
+            
+    return 0;
+}
