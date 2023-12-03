055ce2d3bc6225e2585dd0d80df2c5917f4b531d
yousefbahutair <yousefnas06@gmail.com>
2023-12-03 11:30:39 +0400

    Nestedloops

--- Nestedloops/Nested copy.cpp
+++ Nestedloops/Nested copy.cpp
@@ -8,13 +8,21 @@ int main() {
     cin >> number_sub;
 
     vector<int> scores(number_sub);
-    for(int i{0}; i<=number_sub; ++i) {
+    for(int i=0; i< number_sub; ++i) {
         cout << "Enter score for subject " << i+1 << ": ";
         cin >> scores[i];
     }
+    cout << "Your histogram:"<< endl;
 
-    for(auto score : scores)
-        cout << score << " ";
+    for(auto score : scores) {
+        for( int i{1}; i<= score; ++i) {
+            if( i%5 ==0)
+                cout << "/";
+            else
+                cout << "-";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
