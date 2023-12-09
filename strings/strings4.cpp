23ed046d8544264d40e64d97ee3e9496992aa7ab
yousefbahutair <yousefnas06@gmail.com>
2023-12-09 20:07:31 +0400

    strings

--- strings/strings4.cpp
+++ strings/strings4.cpp
@@ -3,15 +3,21 @@
 using namespace std;
 
 int main() {
-   string d {"When creating a slide deck, most of the time should be spent on your content and message. A PowerPoint presentation’s visual aid is only as good as how much it empowers the presenter’s message. Based on this premise, our PowerPoint templates are created to help you save time creating your presentation decks. You can focus on your message while our designers generate the slideshow templates most suitable for the scenario you need."};
-   string word;
-   cout << d << "\nEnter word to find: ";
-   cin >> word;
-   size_t position = d.find(word);
-   if (position == string::npos)
-      cout << "Not found" << endl;
-   else
-      cout << "Your word is at position " << position << endl;
-
-   return 0;
+    string d {"When creating a slide deck, most of the time should be spent on your content and message. A PowerPoint presentation’s visual aid is only as good as how much it empowers the presenter’s message. Based on this premise, our PowerPoint templates are created to help you save time creating your presentation decks. You can focus on your message while our designers generate the slideshow templates most suitable for the scenario you need."};
+    string word;
+    
+    cout << d << endl;
+    
+    do {
+        cout << "Enter word to find: ";
+        cin >> word;
+        size_t position = d.find(word);
+        if (position == string::npos) {
+            cout << "not found" << endl;
+        }
+        else {
+            cout << "Your word is at position " << position << endl;
+        }
+    } while (position == string::npos);
+    return 0;
 }
